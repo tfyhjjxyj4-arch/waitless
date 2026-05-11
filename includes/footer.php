39da573ab3fbb1b7734812a750b781d68d36be14
tfyhjjxyj4-arch <aryamalshly554@gmail.com>
2026-05-11 19:48:33 +0300

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,19 @@
+</main>
+
+<footer>
+    <div class="container footer-content">
+        <div class="footer-logo">
+            <i class="fas fa-utensils" style="color: var(--accent);"></i> WaitLess
+        </div>
+        <p>نحن هنا لجعل تجربة تناول الطعام الخاصة بك أكثر سهولة ورقيّاً. احجز طاولتك المفضلة الآن واستمتع بوقتك.</p>
+        <div class="social-links">
+            <a href="#"><i class="fab fa-facebook"></i></a>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+            <a href="#"><i class="fab fa-twitter"></i></a>
+        </div>
+        <p class="copyright">&copy; 2026 جميع الحقوق محفوظة لنظام WaitLess. صنع بكل ❤️ لتجربة أفضل.</p>
+    </div>
+</footer>
+
+</body>
+</html>
